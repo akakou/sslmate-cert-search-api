5ff65ead53addd2ed5baa4fa9492208790a804d4
akakou <akakou571@gmail.com>
2024-05-13 05:08:07 +0900

    feat: basic structure

--- main.go
+++ main.go
@@ -1,1 +1,33 @@
 package sslmate_cert_search_api
+
+import "os"
+
+const SSLMATE_API_URL = "https://api.certspotter.com/v1/issuances"
+
+type SSLMateCert struct {
+	Id      string `json:"id"`
+	RawCert string `json:"cert_der"`
+}
+
+type Query struct {
+	Domain            string
+	IncludeSubdomains bool
+	MatchWildcards    bool
+	After             string
+	Expand            string
+}
+
+type Json []map[string]interface{}
+
+type SSLMateSearchAPI struct {
+	Token string
+}
+
+func New(token string) *SSLMateSearchAPI {
+	return &SSLMateSearchAPI{Token: token}
+}
+
+func Default() *SSLMateSearchAPI {
+	token := os.Getenv("SSLMATE_API_TOKEN")
+	return &SSLMateSearchAPI{Token: token}
+}
