39128738fddc3434b04dbc0fcb473fc13313372b
akakou <akakou571@gmail.com>
2024-05-13 05:18:32 +0900

    feat: basic structures

--- monitor/main.go
+++ monitor/main.go
@@ -2,6 +2,30 @@ package monitor
 
 import api "github.com/akakou/sslmate_cert_search_api"
 
-func main() {
-	api.Default()
+var DefaultQuery = api.Query{
+	IncludeSubdomains: true,
+	MatchWildcards:    true,
+	After:             "",
+}
+
+type Monitor struct {
+	Domains   []string
+	Api       *api.SSLMateSearchAPI
+	BaseQuery *api.Query
+}
+
+func New(domains []string, api *api.SSLMateSearchAPI, base *api.Query) *Monitor {
+	return &Monitor{
+		Domains:   domains,
+		Api:       api,
+		BaseQuery: base,
+	}
+}
+
+func Default(domains []string) *Monitor {
+	return New(
+		domains,
+		api.Default(),
+		&DefaultQuery,
+	)
 }
