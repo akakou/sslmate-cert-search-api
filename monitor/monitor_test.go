3efb0e8042ed91ae934864afb2bdb280635bd87a
akakou <akakou571@gmail.com>
2024-05-14 22:55:06 +0900

    fix: name

--- monitor/monitor_test.go
+++ monitor/monitor_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestNext(t *testing.T) {
 	// TestNext tests the Next method of the Monitor struct.
-	certs, _, err := Default("example.com").Next()
+	certs, _, err := DefaultMonitor("example.com").Next()
 	if err != nil {
 		t.Fatalf("failed to get certs: %v", err)
 	}
